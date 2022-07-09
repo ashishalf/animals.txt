b7d0c07ef44ef5e37e05ed49b95fad358d6692e6
whyisthesheep <77189741+whyisthesheep@users.noreply.github.com>
2022-07-09 12:20:53 +0100

    Update animaltxt.py

--- animaltxt.py
+++ animaltxt.py
@@ -1,10 +1,17 @@
 import pyautogui as pg
-import time
+from time import sleep
 
-time.sleep(10)
-
+n = input("Name: ")
+input("Press ENTER to start")
+sleep(3)
 txt = open('animals.txt')
-a = "Shivam is "
+no = 0
+a = n + " is "
 for i in txt:
-    pg.write(a+i)
+    no = no + 1
+    print("printed: " + a + i + no)
+    pg.write(a + i)
     pg.press("Enter")
+    sleep(0.8)
+
+input("Press ENTER to exit")
